fa2341d3ec197e69988f948dc5ab0293f7cfe042
Ernesto Casaverde <u20221b657@upc.edu.pe>
2024-11-18 16:25:57 -0500

    fix

--- obj/Debug/net8.0/Security.AssemblyInfo.cs
+++ obj/Debug/net8.0/Security.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Security")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f70fcc7c2d55c1b3dd9c75cf1e43c8f712aa5d5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3957f4ede43554afe6757bce9df6a7e4f9fe356")]
 [assembly: System.Reflection.AssemblyProductAttribute("Security")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Security")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
